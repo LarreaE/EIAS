d48f79e8c751ffa9a9058005b3c322d07dbcc5d3
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-09-19 10:04:04 +0200

    [EEE-15] button implemented, alert when singed in added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import {SafeAreaView, Text, StyleSheet, View, Button, Alert} from 'react-native';
 import SplashScreen from './components/splashScreen.tsx';  // Importar SplashScreen
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 function App(): React.JSX.Element {
   const [isSplashVisible, setIsSplashVisible] = useState(true);
@@ -13,13 +14,33 @@ function App(): React.JSX.Element {
     return () => clearTimeout(timer);
   }, []);
 
+  //Google Sign in
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: '1023780976448-99hcl1i028jvliu2kn32u62pvmshna11.apps.googleusercontent.com', // From Firebase Console (still required for Google Sign-In)
+    });
+  }, []);
+
+   // Function to handle Google Sign-In
+   async function onGoogleButtonPress() {
+    try {
+      // Get the user's ID token
+      const userInfo = await GoogleSignin.signIn();
+      console.log('User Info', userInfo);
+      Alert.alert('Signed in!', `Welcome`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
   if (isSplashVisible) {
     return <SplashScreen />;
   }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Your main app content goes here.</Text>
+      <Button title="Google Sign-In" onPress={onGoogleButtonPress} />
     </SafeAreaView>
   );
 }
