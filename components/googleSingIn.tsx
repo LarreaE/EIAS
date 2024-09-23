3b684342b7d666f29b636a7510784808b3439f23
ionpazoos <jon.pazos@ikasle.aeg.eus>
2024-09-23 19:26:05 +0200

    [EEE-61]spinner when sing in  implementation

--- components/googleSingIn.tsx
+++ components/googleSingIn.tsx
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Spinner from './Spinner'; // Importa el Spinner
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import Config from 'react-native-config';
 
 interface Props {
   setIsLoged: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const GoogleSignInComponent: React.FC<Props> = ({ setIsLoged }) => {
+  const [loading, setLoading] = useState(false); // Estado para el loading
+
+  useEffect(() => {
+    const configureGoogleSignIn = async () => {
+      await GoogleSignin.configure({
+        webClientId: Config.GOOGLE_WEB_CLIENT_ID, // Asegúrate de tener esto configurado
+        offlineAccess: true,
+      });
+    };
+    
+    configureGoogleSignIn();
+  }, []);
+
   const signIn = async () => {
     try {
-      // Lógica de inicio de sesión
+      setLoading(true); // Iniciar el loading
+      await GoogleSignin.signIn(); // Lógica de inicio de sesión
       setIsLoged(true); // Actualiza el estado de autenticación
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false); // Detener el loading
     }
   };
 
   return (
     <View style={styles.outerContainer}>
-      <TouchableOpacity onPress={signIn}>
+    {loading && <Spinner />}
+    <TouchableOpacity onPress={signIn} disabled={loading}>
       <View style={styles.container}>
-        <Text style={styles.text}>Sing in with google</Text>
+        <Text style={styles.text}>Sign in with Google</Text>
       </View>
-      </TouchableOpacity>
-    </View>
+    </TouchableOpacity>
+  </View>
   );
 };
 
@@ -35,8 +55,8 @@ const styles = StyleSheet.create({
   },
   container: {
     backgroundColor: 'blue',        // Fondo azul
-    width: 200,                     // Ancho de 300 unidades
-    height: 100,                    // Alto de 400 unidades
+    width: 200,                     // Ancho de 200 unidades
+    height: 100,                    // Alto de 100 unidades
     justifyContent: 'center',       // Centrar contenido verticalmente
     alignItems: 'center',           // Centrar contenido horizontalmente
     borderRadius: 20,               // Esquinas redondeadas
