91b920be56a8e28a8367cadc294c56353578d1ac
ionpazoos <jon.pazos@ikasle.aeg.eus>
2024-09-26 19:09:01 +0200

    [EEE-70] fix spinner when log in

--- App.tsx
+++ App.tsx
@@ -9,12 +9,14 @@ import AcolythLaboratoryScreen from './components/acolythLaboratoryScreen.tsx';
 import GoogleSignInComponent from './components/googleSingIn.tsx';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import Config from 'react-native-config';
+import Spinner from './components/Spinner'; // Importa el Spinner
 
 const Tab = createBottomTabNavigator();
 
 function App() {
   const [isLoged, setIsLoged] = useState<boolean>(false);
   const [UserData, setUserData] = useState<any>(null);
+  const [loading, setLoading] = useState(false); // Estado para el loading
 
   useEffect(() => {
     SplashScreen.hide();
