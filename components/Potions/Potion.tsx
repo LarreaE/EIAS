cdbe08d584a820b26344399c3741f67d748e077f
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-10-16 10:42:12 +0200

    [EEE-134][ADD] adds create function(in progress)

--- components/Potions/Potion.tsx
+++ components/Potions/Potion.tsx
@@ -30,7 +30,7 @@ class Potion implements Potions{
         this.value = value;
     }
 
-    static with(ingredients: [Ingredients]){
+    static create(ingredients: [Ingredients]){
 
         let value;
         let description;
@@ -38,14 +38,66 @@ class Potion implements Potions{
         let type;
         let modifiers;
 
-        let effect_magnitude;   // to save the effect strength (lesser, greater ...)
-        let effect_name;        // to save the effect name (restore, increase, ...)
-        let effect_attribute;   // to save what attribute is aimed to (strength, int,...)
+        let effectsArray = [];
+
+        for (let i = 0; i < ingredients.length; i++) {
+            const categorizedEffect = categorizeEffect(ingredients[i].effects);
+            effectsArray.push(categorizedEffect);
+        }
+
+        checkIngredientCompatibility(effectsArray);
 
         const potion_name = `Potion of ${ingredients[0].effects}`;
-        return new Potion()
+        return new Potion();
     }
 
 }
 
+function  categorizeEffect(str: string) {
+
+    const parts = str.split('_');
+    let potency = '';
+    let effect = '';
+    let attribute = '';
+    if (parts.length === 3) {
+        // three parts
+        potency = parts[0];
+        effect = parts[1];
+        attribute = parts[2];
+    } else if (parts.length === 2) {
+        // no potency
+        potency = 'nothing';
+        effect = parts[0];
+        attribute = parts[1];
+    } else {
+        throw new Error('Invalid string format');
+    }
+
+    return { potency, effect, attribute };
+}
+
+const potencyTiers = {
+    least: 0,
+    lesser: 1,
+    nothing: 2,
+    greater: 3,
+};
+
+function checkIngredientCompatibility(effects: Array) {
+
+    for (let i = 0; i < effects.length; i++) {
+        for (let j = i + 1; j < effects.length; j++) {
+          const effect1 = effects[i];
+          const effect2 = effects[j];
+
+          if (effect1.effect === effect2.effect && effect1.attribute === effect2.attribute) {
+
+            const lowerPotencyeffect = potencyTiers[effect1.potency] < potencyTiers[effect2.potency] ? effect1 : effect2;
+            console.log(
+              `Triggering action for effect: ${effect1.effect} and attribute: ${effect1.attribute}. Lower potency is: ${lowerPotencyeffect.potency}`
+            );
+          }
+        }
+    }
+}
 export default Potion;
