6a7d642f44a650a85ca39fb58a216bcfd192eadc
LarreaE <eneko.larrea@ikasle.aeg.eus>
2024-10-21 13:01:07 +0200

    [EEE-167][ADD] added back button for prev curse and styled

--- components/CookBookModal.tsx
+++ components/CookBookModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal, StyleSheet, Text, TouchableOpacity, View, ScrollView, ImageBackground } from "react-native";
+import { Modal, StyleSheet, Text, TouchableOpacity, View, ScrollView, ImageBackground, Dimensions } from "react-native";
 import { Curses } from "../interfaces/Curse";
 
 interface Props {
@@ -7,17 +7,22 @@ interface Props {
     visible: boolean,
     curses: Curses[],
 }
-const CookBookModal: React.FC<Props> = ({visible, setVisible, curses}) => {
 
+const { width, height } = Dimensions.get('window'); // Get device dimensions
+
+const CookBookModal: React.FC<Props> = ({ visible, setVisible, curses }) => {
     const [index, setIndex] = useState(0);
+
     const nextIndex = () => {
-        setIndex(index + 1);
+        setIndex((prevIndex) => (prevIndex + 1) % curses.length); // Loop to the first curse
+    };
+
+    const prevIndex = () => {
+        setIndex((prevIndex) => (prevIndex - 1 + curses.length) % curses.length); // Loop to the last curse
     };
 
     if (!curses[0]) {
-        return (
-            <Text> Receiving Curses</Text>
-        );
+        return <Text>Receiving Curses...</Text>;
     }
 
     return (
@@ -30,118 +35,139 @@ const CookBookModal: React.FC<Props> = ({visible, setVisible, curses}) => {
             }}
         >
             <ImageBackground
-                source={require('../assets/runa.png')}  // Ruta de la imagen
-                style={styles.background}  // Aplicar estilos al contenedor
-                resizeMode="cover"         // Ajuste de la imagen
+                source={require('../assets/runa.png')}
+                style={styles.background}
+                resizeMode="cover"
             >
                 <View style={styles.modalView}>
-                <ScrollView>
-                    <View style={styles.centeredView}>
-                    {
-                        <>
-                            <Text style={styles.textStyle}>{curses[index].name}</Text>
-                            <Text style={styles.textStyle}>{curses[index].description}</Text>
-                            <Text style={styles.textStyle}>{curses[index].poison_effects}</Text>
+                    <ScrollView>
+                        <View style={styles.contentContainer}>
+                            {/* Title and description */}
+                            <Text style={styles.title}>{curses[index].name}</Text>
+                            <Text style={styles.description}>{curses[index].description}</Text>
 
-                        </>
-                    }
+                            {/* Table structure for inflicting and curing curses */}
+                            <View style={styles.tableContainer}>
+                                {/* Row for inflicting */}
+                                <View style={styles.tableRow}>
+                                    <Text style={styles.tableHeader}>How to Inflict</Text>
+                                    <Text style={styles.tableData}>{curses[index].poison_effects}</Text>
+                                </View>
+                                {/* Row for curing */}
+                                <View style={styles.tableRow}>
+                                    <Text style={styles.tableHeader}>How to Cure</Text>
+                                    <Text style={styles.tableData}>{curses[index].antidote_effects}</Text>
+                                </View>
+                            </View>
+                        </View>
+                    </ScrollView>
 
+                    {/* Buttons for navigating between curses */}
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity style={styles.button} onPress={prevIndex}>
+                            <Text style={styles.buttonText}>Back</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.button} onPress={nextIndex}>
+                            <Text style={styles.buttonText}>Next</Text>
+                        </TouchableOpacity>
                     </View>
-                </ScrollView>
-                <TouchableOpacity
-                    style={styles.closeButton}
-                    onPress={nextIndex}
-                >
-                    <Text>Next</Text>
-                </TouchableOpacity>
-                </View>
-                <TouchableOpacity
-                    style={styles.closeButton}
-                    onPress={() => setVisible(false)}
-                >
-                    <Text>Close</Text>
-                </TouchableOpacity>
-            </ImageBackground>
 
+                    {/* Close Button */}
+                    <TouchableOpacity style={styles.closeButton} onPress={() => setVisible(false)}>
+                        <Text style={styles.buttonText}>Close</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
         </Modal>
     );
-
 };
 
-
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-},
-background: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-},
-openButton: {
-    padding: 10,
-    borderRadius: 10,
-    width: 66,
-    height: 66,
-    alignItems: 'center',
-},
-textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-},
-centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-},
-modalView: {
-    width: 300,
-    height: 400,
-    margin: 20,
-    borderRadius: 20,
-    padding: 5,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-},
-modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
-    fontSize: 18,
-},
-closeButton: {
-    backgroundColor: '#2196F3',
-    borderRadius: 10,
-    padding: 10,
-    marginTop: 10,
-},
-image: {
-    width: '40%',
-    height: '40%',
-    color: 'black',
-},
-cookBookButton: {
-    position: 'absolute',
-    top: 50,
-    left: 40,
-    padding: 10,
-    borderRadius: 8,
-    flexDirection: 'row',
-    alignItems: 'center',
-},
-filterModalView: {
-    width: '110%',
-    maxHeight: '80%',
-    padding: 20,
-},
-scrollView: {
-    height:300,
-},
+    background: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalView: {
+        width: width * 0.9, // 90% of screen width
+        height: height * 0.8, // 80% of screen height
+        padding: 20,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        borderRadius: 20,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    contentContainer: {
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+        marginBottom: 15,
+    },
+    description: {
+        fontSize: 16,
+        color: 'white',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    tableContainer: {
+        width: '100%',
+        borderWidth: 1,
+        borderColor: '#ffffff50',
+        borderRadius: 10,
+        padding: 10,
+    },
+    tableRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    tableHeader: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#FFD700', // Gold color for headers
+        textAlign: 'left',
+    },
+    tableData: {
+        fontSize: 16,
+        color: 'white',
+        textAlign: 'right',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginTop: 10,
+    },
+    button: {
+        backgroundColor: '#2196F3',
+        borderRadius: 10,
+        padding: 10,
+        marginHorizontal: 10,
+        width: '40%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    closeButton: {
+        backgroundColor: '#2196F3',
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 10,
+        width: '60%',
+        alignItems: 'center',
+    },
 });
 
 export default CookBookModal;
